210df30ac69778b8e307e1b879a2904f5c384514
gulfem <33688162+avagigi@users.noreply.github.com>
2020-12-14 20:15:11 +0300

    Add files via upload

---
+++ proje.java
@@ -0,0 +1,34 @@
+package proje;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+public class proje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Queue kuyurk=new Queue(13);
+        Queue kuyruk = new Queue(3);
+        Asansor mainAsansor=new Asansor();
+        Asansor asansor2 =new Asansor();
+        Asansor asansor3=new Asansor();
+        Asansor asansor4=new Asansor();
+        Asansor asansor5=new Asansor();
+        kuyruk.put(5);
+        kuyruk.put(4);
+        kuyruk.put(5);
+         
+        // Bundan sonra eklenecek elemanlar için Kuyruk dolu diyecektir
+        
+    }
+    
+}
